ffc91396969026e0c460a2508ab535e918af8c8f
Jerry Trueba <jerrytrueba10@gmail.com>
2022-08-17 21:17:18 -0700

    updated

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Header from './components/Header';
 import Second from './components/Second';
 import Section from './components/Section';
-import Testimonials from './components/Testimonials';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
      <Header/>
      <Section/>
      <Second/>
-     <Testimonials/>
     </div>
   );
 }
