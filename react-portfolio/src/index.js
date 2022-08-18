36328259cae45fe9cec387bd4bac73fa86d56234
Jerry Trueba <jerrytrueba10@gmail.com>
2022-08-17 21:56:01 -0700

    updates

--- react-portfolio/src/index.js
+++ react-portfolio/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import App from './App';
 import Contact from './components/Contact';
 
-const root = React.DOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+
+  <Router>
+    <Routes>
+      <Route path='/' element={<App/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+    </Routes>
+  </Router>,
+  document.getElementById('root')
 );
