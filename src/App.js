ff39ff04cae4cb8f4b686a159f8974a755b41b85
Jerry Trueba <jerrytrueba10@gmail.com>
2022-08-17 20:10:35 -0700

    updated

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './components/Header';
+import Second from './components/Second';
+import Section from './components/Section';
+import Testimonials from './components/Testimonials';
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Section/>
+     <Second/>
+     <Testimonials/>
+    </div>
+  );
+}
+
+export default App;
